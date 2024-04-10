98223f1f612ba618587b90cf8701e2edf65688ca
SangyunOck <demibun123@naver.com>
2024-04-10 21:04:05 +0900

    👔 quic server, client spinup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,19 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use p2p::{spin_up_client, spin_up_server};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let _ = spin_up_server(8080).await.unwrap();
+    spin_up_client("localhost".to_string(), "127.0.0.1:8080".to_string()).await.unwrap();
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
