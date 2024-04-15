42704cedb29ad88a9b2efacd6b58cfcbdd96bdfe
magae1 <jjilil6665@gmail.com>
2024-04-16 00:52:12 +0900

    - 사이드바 추가

--- src/mui_theme.ts
+++ src/mui_theme.ts
@@ -7,11 +7,6 @@ export const mui_theme = createTheme({
         variant: "outlined",
       },
     },
-    MuiListItem: {
-      defaultProps: {
-        disablePadding: true,
-      },
-    },
     MuiCard: {
       defaultProps: {
         variant: "outlined",
