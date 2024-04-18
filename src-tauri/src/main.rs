b49442f0e4270633a99da98eff43b7b355a4d4d9
SangyunOck <demibun123@naver.com>
2024-04-19 00:47:12 +0900

    🎨 server, client spinup 방식 변경

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use p2p::{spin_up_client, spin_up_server};
+use p2p::client::spin_up_client;
+use p2p::server::spin_up_server;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -11,8 +12,34 @@ fn greet(name: &str) -> String {
 
 #[tokio::main]
 async fn main() {
-    let _ = spin_up_server(8080).await.unwrap();
-    spin_up_client("localhost".to_string(), "127.0.0.1:8080".to_string()).await.unwrap();
+    let _ = spin_up_server(
+        8080,
+        |_tx| async move {
+            // TODO: do some tx business logic here
+            ()
+        },
+        |_rx| async move {
+            // TODO: do some rx business logic here
+            ()
+        },
+    )
+    .await
+    .unwrap();
+
+    spin_up_client(
+        "localhost".to_string(),
+        "127.0.0.1:8080".to_string(),
+        |_tx| async move {
+            // TODO: do some tx business logic here
+            ()
+        },
+        |_rx| async move {
+            // TODO: do some rx business logic here
+            ()
+        },
+    )
+    .await
+    .unwrap();
 
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
