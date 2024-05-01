7070c3bfc052328b9d9caa3bf3f41b1c96395257
Heidi <heidi@daangn.com>
2024-05-01 17:45:33 +0900

    lint

--- server/src/main/kotlin/com/demstorage/DatabaseConfiguration.kt
+++ server/src/main/kotlin/com/demstorage/DatabaseConfiguration.kt
@@ -1,13 +1,12 @@
 package com.demstorage
 
 import org.jetbrains.exposed.spring.SpringTransactionManager
-import org.springframework.boot.jdbc.DataSourceBuilder
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import javax.sql.DataSource
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
+import javax.sql.DataSource
 
 @Configuration
 class DatabaseConfiguration {
