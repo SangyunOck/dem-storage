61ec8f51ec62a7d9cd250a3564f095930f416d68
Heidi <heidi@daangn.com>
2024-05-04 21:43:33 +0900

    localhost:1402 cors 허용 추가

---
+++ server/src/main/kotlin/com/demstorage/controller/WebConfigurations.kt
@@ -0,0 +1,15 @@
+package com.demstorage.controller
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfigurations : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:1420")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+    }
+}
