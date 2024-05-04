a0c2a25914edba2759577ee45614ef1017c43870
magae1 <jjilil6665@gmail.com>
2024-05-04 16:02:52 +0900

    - 로그인, 회원가입(+완료) 페이지 ui 최종

--- src/layouts/MemberLayout.tsx
+++ src/layouts/MemberLayout.tsx
@@ -1,10 +1,13 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, useMediaQuery, useTheme } from "@mui/material";
 import { Outlet } from "react-router";
 
 function MemberLayout() {
+  const theme = useTheme();
+  const isMaxSize = useMediaQuery(theme.breakpoints.up("lg"));
+
   return (
     <Box sx={{ height: "100vh", flexGrow: 1 }}>
-      <Container maxWidth={"xs"}>
+      <Container maxWidth={isMaxSize ? "sm" : "xs"}>
         <Outlet />
       </Container>
     </Box>
