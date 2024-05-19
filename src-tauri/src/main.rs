ca9a6fd4f3257ea0036b7c2de075397370fdd7fa
Heidi <heidi@daangn.com>
2024-05-19 16:32:16 +0900

    upload command 정의

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -16,88 +16,134 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+async fn upload(
+    nodes: Vec<Node>,
+    path: &str,
+) -> Result<> {
+
+    // 임시; path랑 node 리스트 찍기
+    println!("path: {}", path);
+    for node in &nodes {
+        println!("node: {:?}", node);
+    }
+
+    Ok(())
+
+    // let scheduled_chunks = get_scheduled_chunks(
+    //     path,
+    //     nodes,
+    // ).await?;
+    //
+    // for scheduled_chunk in &scheduled_chunks {
+    //     let (cmd_tx, cmd_rx) = unbounded();
+    //     drop(
+    //         spin_up_client(
+    //             "localhost".to_string(),
+    //             "127.0.0.1:8080".to_string(), // TODO : 여기가 노드의 endpoint 인가
+    //             client_handler::handle_outgoing_stream,
+    //             client_handler::handle_incoming_stream,
+    //             cmd_rx,
+    //         ).await?,
+    //     );
+    //
+    //     let _ = cmd_tx
+    //         .send(Command::Upload(UploadReq {
+    //             peer_id: scheduled_chunk.node.peer_id.toString(),
+    //             password: "password가 뭐임 어디서 들고있는거임",
+    //             // TODO: change storage path
+    //             file_path: scheduled_chunk.chunk.file_path.clone(),
+    //             offset: scheduled_chunk.chunk.offset,
+    //             limit: scheduled_chunk.chunk.chunk_size,
+    //             index: scheduled_chunk.chunk.index,
+    //         }))
+    //         .await;
+    // }
+}
+
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    let port = std::env::var("PORT")
-        .unwrap_or_default()
-        .parse()
-        .unwrap_or(8080);
-
-    drop(
-        spin_up_server(
-            port,
-            server_handler::handle_outgoing_stream,
-            server_handler::handle_incoming_stream,
-            // TODO: change storage path
-            "/Users/sangyun/Documents/workspace/dem-storage/server-storage".to_string(),
-        )
-        .await?,
-    );
-
-    let nodes = vec![
-        Node {
-            endpoint: "".to_string(),
-            peer_id: "1".to_string(),
-        },
-        Node {
-            endpoint: "".to_string(),
-            peer_id: "2".to_string(),
-        },
-        Node {
-            endpoint: "".to_string(),
-            peer_id: "3".to_string(),
-        },
-    ];
-
-    let scheduled_chunks = get_scheduled_chunks(
-        "/Users/sangyun/Documents/workspace/dem-storage/src-tauri/Cargo.toml",
-        nodes,
-    )
-    .await?;
-
-    for scheduled_chunk in &scheduled_chunks {
-        let (cmd_tx, cmd_rx) = unbounded();
-        drop(
-            spin_up_client(
-                "localhost".to_string(),
-                "127.0.0.1:8080".to_string(),
-                client_handler::handle_outgoing_stream,
-                client_handler::handle_incoming_stream,
-                cmd_rx,
-            )
-            .await?,
-        );
-        let _ = cmd_tx
-            .send(Command::Upload(UploadReq {
-                peer_id: "id".to_string(),
-                password: "password".to_string(),
-                // TODO: change storage path
-                file_path: scheduled_chunk.chunk.file_path.clone(),
-                offset: scheduled_chunk.chunk.offset,
-                limit: scheduled_chunk.chunk.chunk_size,
-                index: scheduled_chunk.chunk.index,
-            }))
-            .await;
-
-        let file_name = Path::new(&scheduled_chunk.chunk.file_path)
-            .file_name()
-            .unwrap()
-            .to_str()
-            .unwrap();
-
-        let _ = cmd_tx
-            .send(Command::DownloadReq(DownloadReq {
-                peer_id: "id".to_string(),
-                file_name: file_name.to_string(),
-                index: scheduled_chunk.chunk.index,
-                chunk_offset: scheduled_chunk.chunk.offset,
-                password: "password".to_string(),
-            }))
-            .await;
-    }
+//     let port = std::env::var("PORT")
+//         .unwrap_or_default()
+//         .parse()
+//         .unwrap_or(8080);
+//
+//     drop(
+//         spin_up_server(
+//             port,
+//             server_handler::handle_outgoing_stream,
+//             server_handler::handle_incoming_stream,
+//             // TODO: change storage path
+//             "/Users/sangyun/Documents/workspace/dem-storage/server-storage".to_string(),
+//         )
+//         .await?,
+//     );
+//
+//     let nodes = vec![
+//         Node {
+//             endpoint: "".to_string(),
+//             peer_id: "1".to_string(),
+//         },
+//         Node {
+//             endpoint: "".to_string(),
+//             peer_id: "2".to_string(),
+//         },
+//         Node {
+//             endpoint: "".to_string(),
+//             peer_id: "3".to_string(),
+//         },
+//     ];
+//
+//     let scheduled_chunks = get_scheduled_chunks(
+//         "/Users/sangyun/Documents/workspace/dem-storage/src-tauri/Cargo.toml",
+//         nodes,
+//     )
+//     .await?;
+//
+//     for scheduled_chunk in &scheduled_chunks {
+//         let (cmd_tx, cmd_rx) = unbounded();
+//         drop(
+//             spin_up_client(
+//                 "localhost".to_string(),
+//                 "127.0.0.1:8080".to_string(),
+//                 client_handler::handle_outgoing_stream,
+//                 client_handler::handle_incoming_stream,
+//                 cmd_rx,
+//             )
+//             .await?,
+//         );
+//         let _ = cmd_tx
+//             .send(Command::Upload(UploadReq {
+//                 peer_id: "id".to_string(),
+//                 password: "password".to_string(),
+//                 // TODO: change storage path
+//                 file_path: scheduled_chunk.chunk.file_path.clone(),
+//                 offset: scheduled_chunk.chunk.offset,
+//                 limit: scheduled_chunk.chunk.chunk_size,
+//                 index: scheduled_chunk.chunk.index,
+//             }))
+//             .await;
+//
+//         let file_name = Path::new(&scheduled_chunk.chunk.file_path)
+//             .file_name()
+//             .unwrap()
+//             .to_str()
+//             .unwrap();
+//
+//         let _ = cmd_tx
+//             .send(Command::DownloadReq(DownloadReq {
+//                 peer_id: "id".to_string(),
+//                 file_name: file_name.to_string(),
+//                 index: scheduled_chunk.chunk.index,
+//                 chunk_offset: scheduled_chunk.chunk.offset,
+//                 password: "password".to_string(),
+//             }))
+//             .await;
+//     }
 
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![upload])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 
