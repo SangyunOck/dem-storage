6e1f624caedbab7449e74bd66f3470c8b45f51ba
Heidi <heidi@daangn.com>
2024-05-19 17:19:54 +0900

    다운로드 커맨드 추가

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,7 +7,7 @@ use p2p::client::spin_up_client;
 use p2p::scheduler::get_scheduled_chunks;
 use p2p::server::handler as server_handler;
 use p2p::server::spin_up_server;
-use p2p::types::{Command, DownloadReq, Node, UploadReq};
+use p2p::types::{Command, DownloadReq, Node, ScheduledChunk, UploadReq};
 use std::path::Path;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -20,14 +20,17 @@ fn greet(name: &str) -> String {
 async fn upload(
     nodes: Vec<Node>,
     path: &str,
-) -> Result<> {
+    user_password: &str
+) -> eyre::Result<()> {
+    // 프론트: 파일이 어떤 Vec<ScheduledChunk> 를 썼는지 기억해야한다.
+    // TODO 파일별 청크 저장위해 리턴값 바꿔야함. 바꿀 때 상윤이랑 다시 이야기하기. eyre::Result<Vec<ScheduledChunk>>
 
     // 임시; path랑 node 리스트 찍기
     println!("path: {}", path);
+    println!("user_password: {}", user_password);
     for node in &nodes {
         println!("node: {:?}", node);
     }
-
     Ok(())
 
     // let scheduled_chunks = get_scheduled_chunks(
@@ -40,7 +43,7 @@ async fn upload(
     //     drop(
     //         spin_up_client(
     //             "localhost".to_string(),
-    //             "127.0.0.1:8080".to_string(), // TODO : 여기가 노드의 endpoint 인가
+    //             scheduled_chunk.node.endpoint.to_string(),
     //             client_handler::handle_outgoing_stream,
     //             client_handler::handle_incoming_stream,
     //             cmd_rx,
@@ -49,9 +52,8 @@ async fn upload(
     //
     //     let _ = cmd_tx
     //         .send(Command::Upload(UploadReq {
-    //             peer_id: scheduled_chunk.node.peer_id.toString(),
-    //             password: "password가 뭐임 어디서 들고있는거임",
-    //             // TODO: change storage path
+    //             peer_id: scheduled_chunk.node.peer_id.to_string(),
+    //             password: user_password.to_string(),
     //             file_path: scheduled_chunk.chunk.file_path.clone(),
     //             offset: scheduled_chunk.chunk.offset,
     //             limit: scheduled_chunk.chunk.chunk_size,
@@ -61,6 +63,46 @@ async fn upload(
     // }
 }
 
+#[tauri::command]
+async fn download(
+    scheduled_chunks: Vec<ScheduledChunk>
+) -> eyre::Result<()> {
+
+    for scheduled_chunk in &scheduled_chunks {
+        println!("scheduled_chunk: {:?}", scheduled_chunk);
+    }
+    Ok(())
+
+    // for scheduled_chunk in &scheduled_chunks {
+    //     let (cmd_tx, cmd_rx) = unbounded();
+    //     drop(
+    //         spin_up_client(
+    //             "localhost".to_string(),
+    //             scheduled_chunk.node.endpoint.to_string(),
+    //             client_handler::handle_outgoing_stream,
+    //             client_handler::handle_incoming_stream,
+    //             cmd_rx,
+    //         ).await?,
+    //     );
+    //     let file_name = Path::new(&scheduled_chunk.chunk.file_path)
+    //         .file_name()
+    //         .unwrap()
+    //         .to_str()
+    //         .unwrap();
+    //
+    //     let _ = cmd_tx
+    //         .send(Command::DownloadReq(DownloadReq {
+    //             peer_id: scheduled_chunk.node.peer_id.to_string(),
+    //             file_name: file_name.to_string(),
+    //             index: scheduled_chunk.chunk.index,
+    //             chunk_offset: scheduled_chunk.chunk.offset,
+    //             password: "password".to_string(),
+    //         }))
+    //         .await;
+    // }
+    //
+    // Ok(())
+}
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
 //     let port = std::env::var("PORT")
