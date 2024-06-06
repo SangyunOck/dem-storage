63267dd7c80bee0aae350499b98bced4ddbc2c78
Heidi <heidi@daangn.com>
2024-06-06 16:46:36 +0900

    사용 가능 노드 조회 gRPC endpoint 작성

--- server/src/main/kotlin/com/demstorage/grpc/MainServerService.kt
+++ server/src/main/kotlin/com/demstorage/grpc/MainServerService.kt
@@ -2,7 +2,11 @@ package com.demstorage.grpc
 
 import com.demstorage.node.domain.NodeIpAddress
 import com.demstorage.node.domain.NodePeerId
+import com.demstorage.node.service.AvailableNodeLoader
 import com.demstorage.node.service.NodeOperations
+import com.demstorage.stubs.ListAvailableNodesRequest
+import com.demstorage.stubs.ListAvailableNodesResponse
+import com.demstorage.stubs.ListAvailableNodesResponse.Node
 import com.demstorage.stubs.MainServerOperationsGrpc
 import com.demstorage.stubs.RegisterNodeRequest
 import com.demstorage.stubs.RegisterNodeResponse
@@ -13,6 +17,7 @@ import net.devh.boot.grpc.server.service.GrpcService
 @GrpcService
 class MainServerService(
     private val nodeOperations: NodeOperations,
+    private val availableNodeLoader: AvailableNodeLoader,
 ) : MainServerOperationsGrpc.MainServerOperationsImplBase() {
 
     private val logger = KotlinLogging.logger {}
@@ -29,6 +34,27 @@ class MainServerService(
         )
 
         val response = RegisterNodeResponse.newBuilder().build()
+        responseObserver.onNext(response)
+        responseObserver.onCompleted()
+    }
+
+    override fun listAvailableNodes(
+        request: ListAvailableNodesRequest,
+        responseObserver: StreamObserver<ListAvailableNodesResponse>
+    ) {
+        logger.info { "get available nodes" }
+        val nodes = availableNodeLoader.load(limit = 3)
+
+        val response = ListAvailableNodesResponse.newBuilder().apply {
+            nodes.forEach {
+                addNodes(
+                    Node.newBuilder().apply {
+                        setNodeIp(it.ipAddress.value)
+                        setPeerId(it.peerId.value)
+                    }
+                )
+            }
+        }.build()
         responseObserver.onNext(response)
         responseObserver.onCompleted()
     }
